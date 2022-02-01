8d610d6bc016c1eeb23ac511714befea574e1450
karnas01 <ekremkarnas01@gmail.com>
2022-02-01 15:11:20 +0300

    Win, Fail, InGame UI's Added.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -7,16 +7,13 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] CanvasGroup canvasGroupFail;
     [SerializeField] CanvasGroup canvasGroupWon;
+    [SerializeField] CanvasGroup canvasGroupMainUI;
     private Tween fadeTween;
     [SerializeField] GameObject MainUI;
     [SerializeField] GameObject InGameUI;
     [SerializeField] GameObject FailUI;
     [SerializeField] GameObject WinUI;
 
-    void Start()
-    {
-        // StartCoroutine(TestFade());
-    }
 
     void Update()
     {
@@ -36,25 +33,7 @@ public class UIManager : MonoBehaviour
     public void FadeOut(float duration, CanvasGroup canvasGroup)
     {
         Fade(0f, duration, () => { canvasGroup.interactable = false; canvasGroup.blocksRaycasts = false;}, canvasGroup);
-
-
-        // x(0f,6f, "sadas", () =>{
-        //     Debug.Log("sdds")
-        // });
-
-
-
-        // var val1 = 0f;
-        // var val2 = 6f;
-        // var str1 = "sadas";
-
-        // x(val1, val2, str1, SampleFunc);
     }
-
-    // void SampleFunc()
-    // {
-    //      Debug.Log("sdds")
-    // }
 
     private void Fade(float endValue, float duration, TweenCallback onEnd, CanvasGroup canvasGroup)
     {
@@ -68,7 +47,8 @@ public class UIManager : MonoBehaviour
 
     public void StartGameButton()
     {
-        MainUI.SetActive(false);
+        // MainUI.SetActive(false);
+        FadeOut(0.5f, canvasGroupMainUI);
         InGameUI.SetActive(true);
     }
 
@@ -77,7 +57,7 @@ public class UIManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))
         {
             FailUI.SetActive(true);
-            FadeIn(0.5f, canvasGroupFail);
+            FadeIn(0.25f, canvasGroupFail);
         }
     }
 
@@ -90,11 +70,13 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    // private IEnumerator TestFade()
-    // {
-    //     yield return new WaitForSeconds(2f);
-    //     FadeOut(0.5f);
-    //     yield return new WaitForSeconds(2f);
-    //     FadeIn(0.5f, canvasGroup);
-    // }
+    public void NextLevelButton()
+    {
+
+    }
+
+    public void TryAgainButton()
+    {
+        
+    }
 }
