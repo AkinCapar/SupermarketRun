9aecdec2306a34b57220c066f25420b404be436d
karnas01 <ekremkarnas01@gmail.com>
2022-02-04 17:43:34 +0300

    Some improvements for FinishScene.

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -8,6 +8,10 @@ public class AnimatorController : MonoBehaviour
     public Animator animPingPong;
      public Animator animDance;
     [SerializeField] GameObject shineEffect;
+    [SerializeField] GameObject confettiParticleEffect1;
+    [SerializeField] GameObject confettiParticleEffect2;
+    [SerializeField] GameObject cooker;
+    [SerializeField] private Cinemachine.CinemachineVirtualCamera firstCamera;
     void Start()
     {
         StartCoroutine(PlayAnim());
@@ -33,7 +37,19 @@ public class AnimatorController : MonoBehaviour
         yield return new WaitForSeconds(1.8f);
         animOpen.SetBool("Open", true);
         yield return new WaitForSeconds(1.5f);
-        animDance.SetBool("Dance",true);
+        // animDance.SetBool("Dance",true);
         shineEffect.SetActive(true);
+        confettiParticleEffect1.SetActive(true);
+        confettiParticleEffect2.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+
+        // camera degis
+        firstCamera.enabled = false;
+         yield return new WaitForSeconds(0.5f);
+        cooker.SetActive(true);
+        animDance.SetBool("Dance",true);
+        shineEffect.transform.position = new Vector3(0.04f, 0.04f, -2.9f);
+        
     }
 }
